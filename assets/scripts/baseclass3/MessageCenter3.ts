35391fa2f629e10f3a677103d61eb114fe760115
RionaHomePC <2489339078@qq.com>
2024-12-18 21:35:06 +0800

    20241218 2134 下一步该让箭头能够跟着走了

--- assets/scripts/baseclass3/MessageCenter3.ts
+++ assets/scripts/baseclass3/MessageCenter3.ts
@@ -10,17 +10,17 @@ export class MessageCenter3 {
 
 
     // --------工厂生产线<场景名，自己instance>
-    protected static Map_Instance = new Map<string, MessageCenter3>();
+    protected static Map_Instance = new Map<string, MessageCenter_Scene>();
 
     // 创建一个Instance，并且加入生产线队列，注意，这个外部不调用
     static createInstance(scenename1:string)
     {
-        let tmp_instance = new MessageCenter3();
+        let tmp_instance = new MessageCenter_Scene(scenename1);
         MessageCenter3.Map_Instance.set(scenename1, tmp_instance)
     }
 
     // 调取某个场景的MessageCenter3
-    static getInstance(scenename1:string) : MessageCenter3
+    static getInstance(scenename1:string) : MessageCenter_Scene
     {
         if(!MessageCenter3.Map_Instance.has(scenename1))
         {
@@ -34,13 +34,29 @@ export class MessageCenter3 {
     // 使用的办法是MessageCenter3.getInstance("场景1").RegisterReceiver()
 
 
-    //---------- 消息中心Instance独有部分，非静态
+   
+
+
+}
+
+export class MessageCenter_Scene
+{
+
+ //---------- 消息中心Instance独有部分，非静态
     // GObjectbase1对象Hash表,<对象名称，instance>
-    Hash_map = new Map<string, GObjectbase1>();
+    Hash_map:Map<string, GObjectbase1> = new Map<string, GObjectbase1>();
+    scenename:string;
+
+
+    constructor(scenename:string)
+    {
+        this.scenename = scenename;
+    }
 
     // 注册消息监听
     RegisterReceiver(obj_name:string, node1: GObjectbase1) {
         this.Hash_map.set(obj_name, node1)
+        // console.log(this.scenename +"收到注册"+obj_name)
     }
 
     // 组装一个消息，然后发送给对应的对象
@@ -53,7 +69,6 @@ export class MessageCenter3 {
             this.Hash_map.get(i_receiver).ReceiveMessage(msg1)
         }
     }
-
 
 }
 
