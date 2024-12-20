ac7bbd34506a76f24733cbb0abd41cdeda9566ef
RionaHomePC <2489339078@qq.com>
2024-12-20 22:37:19 +0800

    20241220 2236箭头可以变色了，欠缺筛选可碰撞塔

--- assets/scripts/Battle1/LineArrow_Controller.ts
+++ assets/scripts/Battle1/LineArrow_Controller.ts
@@ -1,7 +1,8 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Collider2D, Color, Component, Contact2DType, IPhysics2DContact, Node, Sprite, Vec3 } from 'cc';
 import { Message3 } from '../baseclass3/Message3';
 import { MessageCenter3 } from '../baseclass3/MessageCenter3';
 import { GObjectbase1 } from '../baseclass3/GObjectbase1';
+import { TowerNode_Controller } from './TowerNode_Controller';
 const { ccclass, property } = _decorator;
 
 @ccclass('LineArrow_Controller')
@@ -18,12 +19,19 @@ export class LineArrow_Controller extends GObjectbase1 {
     end_wx: number = -1;     // 箭头终止绘图点世界坐标
     end_wy: number = -1;
 
+
+    block_end = false;   // 是否锁住终点
+    lock_endx: number = -1;
+    lock_endy: number = -1;
+
     readonly arrow_width: number = 180 + 15;  // 原始的箭头宽度
 
 
-    arrow_head: Node = null;
-    arrow_body: Node = null;
+    arrow_head: Node = null;    // 箭头
+    arrow_body: Node = null;    // 箭头
 
+
+    local_collider: Collider2D = null; // 箭头尖尖的小碰撞体
 
     // ----- 重载
     // 设置自己接受消息的类型，等待继承重写。
@@ -38,6 +46,13 @@ export class LineArrow_Controller extends GObjectbase1 {
 
 
 
+    protected onDestroy(): void {
+        if (this.local_collider) {
+            this.local_collider.off(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            this.local_collider.off(Contact2DType.END_CONTACT, this.onEndContact, this);
+        }
+    }
+
 
     start() {
         // 注册messagecenter
@@ -47,7 +62,14 @@ export class LineArrow_Controller extends GObjectbase1 {
         this.arrow_head = this.node.children[0];
         this.arrow_body = this.node.children[1];
         this.arrow_body.setScale(0, 0, 1)   // 把箭头隐藏掉
-        this.arrow_head.setScale(0,0,1) // 把箭头隐藏掉
+        this.arrow_head.setScale(0, 0, 1) // 把箭头隐藏掉
+
+        // 碰撞体
+        this.local_collider = this.getComponent(Collider2D);
+        if (this.local_collider) {
+            this.local_collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            this.local_collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+        }
     }
 
     update(deltaTime: number) {
@@ -81,7 +103,9 @@ export class LineArrow_Controller extends GObjectbase1 {
         this.end_wx = w_x;
         this.end_wy = w_y;
         this.bStartConnect = true;   // 开始作图模式
-        this.arrow_head.setScale(1,1,1)
+        this.arrow_head.setScale(1, 1, 1)  //放出箭头
+
+        this._ArrowColorRed()  // 变更为红色
     }
 
     // 随着鼠标移动，改变终止绘图点
@@ -94,8 +118,66 @@ export class LineArrow_Controller extends GObjectbase1 {
     StopDraw() {
         this.bStartConnect = false;   // 关闭作图模式
         this.arrow_body.setScale(0, 0, 1)
-        this.arrow_head.setScale(0,0,1)
+        this.arrow_head.setScale(0, 0, 1)
     }
+
+
+    // 锁住箭头终点（为了制造磁吸效果）
+    private _lock_end(endpoint: Vec3) {
+        this.block_end = true;
+        this.lock_endx = endpoint.x;
+        this.lock_endy = endpoint.y;
+    }
+    // 解锁箭头终点
+    private _unlock_end() {
+        this.block_end = false;
+    }
+
+    // 箭头变红
+    private _ArrowColorRed() {
+        this.arrow_head.getComponent(Sprite).color = new Color().fromHEX("#AD2014");
+        this.arrow_body.getComponent(Sprite).color = new Color().fromHEX("#AD2014");
+    }
+
+    // 箭头变蓝
+    private _ArrowColorBlue() {
+        this.arrow_head.getComponent(Sprite).color = new Color().fromHEX("#143FAD");
+        this.arrow_body.getComponent(Sprite).color = new Color().fromHEX("#143FAD");
+    }
+    // 碰撞回调
+    onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+
+        // 如果碰撞的是塔
+        let tower_com = otherCollider.getComponent(TowerNode_Controller)
+        if (tower_com != null) {
+
+            // 判断能不能碰撞
+            let bAllowConnect = 未完成;
+
+            // 能碰撞就锁死 变蓝
+            if (bAllowConnect) {
+                this._lock_end(tower_com.node.getWorldPosition())
+                this._ArrowColorBlue();
+            }
+            else //不能碰撞就箭头标红
+            {
+                this._ArrowColorRed();
+            }
+
+            return
+        }
+
+    }
+
+    // 碰撞回调
+    onEndContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+
+        // 箭头标红
+        this._ArrowColorRed();
+    }
+
+
+
 }
 
 
